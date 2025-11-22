096383df5ff26783e10626bfeeb511cceb5e0d08
Hikmat Tatari <hikmatt1993@gmail.com>
2025-11-22 16:46:34 +0100

    setup project
    Changes on the used template project
    Add boostrap

--- books-test.client/src/app/app.ts
+++ books-test.client/src/app/app.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,23 +8,10 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  public forecasts: WeatherForecast[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
   }
 
   protected readonly title = signal('books-test.client');
